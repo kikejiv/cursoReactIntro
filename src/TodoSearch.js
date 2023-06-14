d609e6bbb2922cb36154158147e7f51fab1bac27
Jose Valencia <tecnicojiv@gmail.com>
2023-06-13 22:41:18 -0500

    paso-4 enventos en REact onClick - onChange

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,7 +1,19 @@
-import './TodoSearch.css'
+import React from 'react';
+import './TodoSearch.css';
+
 function TodoSearch() {
+  
   return (
-    <input placeholder="Buscar algo" className="TodoSearch" />
+    <input placeholder="Buscar algo" 
+    className="TodoSearch" 
+    onChange={(event) => {
+      console.log("escribistes en el serch")
+      console.log(event) 
+      console.log(event.target) 
+      console.log(event.target.value) 
+
+    }} 
+    />
   );
 }
 
